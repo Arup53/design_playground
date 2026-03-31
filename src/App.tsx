86a4d8e9697f109028e213168e111e88aae0a195
Arup53 <arupkumarhowlader12@gmail.com>
2026-04-01 00:04:43 +0600

    feat: run FirstExample component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import StackCards from "./components/1.stack_cards/stackCards";
+import FirstExample from "./components/2.Transition/FirstExample";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen w-full">
+    <div className="flex flex-col gap-6 justify-center items-center h-screen w-full">
       <StackCards />
+      <FirstExample />
     </div>
   );
 }
