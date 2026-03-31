f133f862c36ee3a617c691ba3d20505c0ae5b29d
Arup53 <arupkumarhowlader12@gmail.com>
2026-03-31 20:42:13 +0600

    feat: use StackCards component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import "./index.css";
-
-type CSSVariables = {
-  [key: `--${string}`]: string | number;
-};
+import StackCards from "./components/1.stack_cards/stackCards";
 
 function App() {
-  const LENGTH = 3;
-
   return (
     <div className="flex justify-center items-center h-screen w-full">
-      <div className="wrapper">
-        {new Array(LENGTH).fill(0).map((_, i) => (
-          <div
-            className="card"
-            key={i}
-            style={
-              { "--index": LENGTH - 1 - i } as React.CSSProperties &
-                CSSVariables
-            }
-          />
-        ))}
-      </div>
+      <StackCards />
     </div>
   );
 }
