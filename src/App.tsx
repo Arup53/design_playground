5225b4cf2356214336ae3206e4365cedc645e020
Arup53 <arupkumarhowlader12@gmail.com>
2026-04-01 11:05:05 +0600

    feat: run SecondExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import StackCards from "./components/1.stack_cards/stackCards";
 import FirstExample from "./components/2.Transition/FirstExample";
+import SecondExample from "./components/2.Transition/SecondExample";
 
 function App() {
   return (
     <div className="flex flex-col gap-6 justify-center items-center h-screen w-full">
-      <StackCards />
+      {/* <StackCards />
       <FirstExample />
+      <></> */}
+      <SecondExample />
     </div>
   );
 }
