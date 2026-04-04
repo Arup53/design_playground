6db388e1357ce6f5c1b3b89772d341fad0bfa463
Arup53 <arupkumarhowlader12@gmail.com>
2026-04-04 23:13:31 +0600

    feat:implement SecondExampleKeyframe component

---
+++ src/components/3.keyframe/secondExample/SecondExampleKeyframe.tsx
@@ -0,0 +1,11 @@
+import "./SecondExampleStyles.css";
+const SecondExampleKeyframe = () => {
+  return (
+    <div className="parent">
+      <div className="circle" />
+      <div className="orbitingCircle" />
+    </div>
+  );
+};
+
+export default SecondExampleKeyframe;
