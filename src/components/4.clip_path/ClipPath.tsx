cb4aab13f2d244be8f9ecb231c338ea276c86a58
Arup53 <arupkumarhowlader12@gmail.com>
2026-04-13 13:39:18 +0600

    feat: implement clipath with two image

---
+++ src/components/4.clip_path/ClipPath.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+const ClipPath = () => {
+  return (
+    <div className="container">
+      <img className="img1" src="/download_1.webp" alt="" />
+      <img className="img2" src="/download.webp" alt="" />
+    </div>
+  );
+};
+
+export default ClipPath;
