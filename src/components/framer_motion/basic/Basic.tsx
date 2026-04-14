5ed25c5e2ea1a1a064c59dee467332aa242b4039
Arup53 <arupkumarhowlader12@gmail.com>
2026-04-14 10:49:15 +0600

    feat: create spring vs ease example

--- src/components/framer_motion/basic/Basic.tsx
+++ src/components/framer_motion/basic/Basic.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import "./style.css";
+import { motion } from "motion/react";
 
 const Basic = () => {
-  return <div>Basic</div>;
+  const [reRun, setReRun] = useState(1);
+  return (
+    <div className="wrapper">
+      <motion.div
+        initial={{ x: -500 }}
+        animate={{
+          x: 500,
+          transition: { type: "spring", duration: 2, bounce: 0.2 },
+        }}
+        className="element"
+      />
+      <motion.div
+        className="element"
+        initial={{ x: 500 }}
+        animate={{
+          x: -500,
+          transition: { duration: 2, ease: "easeOut" },
+        }}
+      />
+    </div>
+  );
 };
 
 export default Basic;
