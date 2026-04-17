4e8fa745c14a246eafa13c97e2ec47ef9a15f8fd
Arup53 <arupkumarhowlader12@gmail.com>
2026-04-17 12:22:32 +0600

    feat: implement exitanimation example

--- src/components/framer_motion/basic/Basic.tsx
+++ src/components/framer_motion/basic/Basic.tsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import "./style.css";
 import { AnimatePresence, motion } from "motion/react";
+import { CheckCheckIcon, CopyIcon } from "lucide-react";
 
 const Basic = () => {
   const [isVisible, setIsVisible] = useState(true);
+
+  // for example 3
+  const variants = {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 },
+  };
+
+  const [copied, setCopied] = useState(false);
+
   return (
     <div className="wrapper">
-      {/* spring vs easing */}
-      <motion.div
+      {/*1. spring vs easing */}
+      {/* <motion.div
         initial={{ x: -500 }}
         animate={{
           x: 500,
@@ -22,10 +32,10 @@ const Basic = () => {
           x: -500,
           transition: { duration: 2, ease: "easeOut" },
         }}
-      />
+      /> */}
 
-      {/* exit animation when a component is unmount from view */}
-      <AnimatePresence>
+      {/*2. exit animation when a component is unmount from view */}
+      {/* <AnimatePresence>
         {isVisible ? (
           <motion.div
             initial={{ opacity: 0 }}
@@ -35,7 +45,39 @@ const Basic = () => {
             className="element cursor-pointer"
           />
         ) : null}
-      </AnimatePresence>
+      </AnimatePresence> */}
+
+      {/* 3.exitanimation example: before and after copy button exit animation */}
+
+      <button
+        className="cursor-pointer border border-gray-300 rounded-md h-[32px] w-[32px] flex justify-center items-center p-[6px]"
+        aria-label="Copy code snippet"
+        onClick={() => setCopied(true)}
+      >
+        <AnimatePresence mode="wait" initial={false}>
+          {copied ? (
+            <motion.span
+              key="checkmark"
+              variants={variants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              <CheckCheckIcon size={15} />
+            </motion.span>
+          ) : (
+            <motion.span
+              key="copy"
+              variants={variants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              <CopyIcon size={15} />
+            </motion.span>
+          )}
+        </AnimatePresence>
+      </button>
     </div>
   );
 };
